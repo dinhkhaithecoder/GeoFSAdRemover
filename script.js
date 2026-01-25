7de59814596044df9a9656715efd39fe7e2700b4
Windows11TheCoder <ldkhai3011@gmail.com>
2026-01-25 13:51:47 +0700

    Delete script.js

--- script.js
+++
@@ -1,7 +0,0 @@
-function removeElementsByClass(className){
-    const elements = document.getElementsByClassName(className);
-    while(elements.length > 0){
-        elements[0].parentNode.removeChild(elements[0]);
-    }
-}
-removeElementsByClass("geofs-adbanner geofs-adsense-container");
