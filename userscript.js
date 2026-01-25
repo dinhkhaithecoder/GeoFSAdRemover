d794043ce3db608bb9888c8238d2242fa041276a
Windows11TheCoder <ldkhai3011@gmail.com>
2026-01-25 13:55:32 +0700

    Update userscript.js

--- userscript.js
+++ userscript.js
@@ -1,8 +1,8 @@
 // ==UserScript==
-// @name         GeoFS GPWS Alerts
-// @namespace    https://avramovic.info/
-// @version      1.0.7
-// @description  GPWS and other alerts for GeoFS
+// @name         GeoFS Ad Remover
+// @namespace    https://sites.google.com/view/websitewin8chanboy/trang-ch%E1%BB%A7
+// @version      1.0.1
+// @description  This addon removes ad bar for better flying experince 
 // @author       Win8ChanBoy
 // @match        https://www.geo-fs.com/geofs.php?v=3.9
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=geo-fs.com
