fe1f0d449faf299b5c554f43638dda7833bfa4b4
Windows11TheCoder <ldkhai3011@gmail.com>
2026-01-25 13:52:27 +0700

    Add files via upload

---
+++ userscript.js
@@ -0,0 +1,17 @@
+// ==UserScript==
+// @name         GeoFS GPWS Alerts
+// @namespace    https://avramovic.info/
+// @version      1.0.7
+// @description  GPWS and other alerts for GeoFS
+// @author       Nemanja Avramovic (Modded sound by Win8ChanBoy)
+// @match        https://www.geo-fs.com/geofs.php*
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=geo-fs.com
+// ==/UserScript==
+
+function removeElementsByClass(className){
+    const elements = document.getElementsByClassName(className);
+    while(elements.length > 0){
+        elements[0].parentNode.removeChild(elements[0]);
+    }
+}
+removeElementsByClass("geofs-adbanner geofs-adsense-container");
